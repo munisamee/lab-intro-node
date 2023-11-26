a2ea942b35416c0df44f6613a28beb14e2de1d16
Munisakhon Ermatova <munisakhonermatova@munisakhons-air.speedport.ip>
2023-11-26 18:57:29 +0100

    lab is done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    // Finds the index
+    const index = this.findInsertIndex(item);
+  
+    // Inserts the item
+    this.items.splice(index, 0, item);
 
-  get(pos) {}
+    // Updates the length property
+    this.length = this.item.length;
 
-  max() {}
+    // Method to find the index
+    findInsertIndex(item) {
+      for (let i = 0; i < this.item.length; i++) {
+        if (item <= this.item[i]) {
+          return i;
+        }
+      }
+    }
+  }
 
-  min() {}
+  get(pos) {
+    if (pos , 0 || pos >= this.elements.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.elements[pos];
+  }
 
-  sum() {}
+  max() {
+    if (this.element.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.elements[this.elements.lenght - 1];
+  }
 
-  avg() {}
+  min() {
+    if (this.elements.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.elements[0];
+  }
+
+  sum() {
+    return this.elements.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  avg() {
+    if (this.elements.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.elements.length;
+  }
 }
 
 module.exports = SortedList;
